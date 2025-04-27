721593e8035823806dcdf61e04a0286ee8f0ef0b
Emilia33223 <s33223@pjwstk.edu.pl>
2025-04-27 12:25:30 +0200

    Project added

---
+++ tutt7/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car(19);
+        myCar.start();
+        myCar.stop();
+        System.out.println("Number of seats: " + myCar.getNumberOfSeats());
+    }
+}
+
