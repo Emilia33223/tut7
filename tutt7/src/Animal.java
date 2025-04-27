5b5cc993d109c25b3d7310981c81802c29ff1fe4
Emilia33223 <s33223@pjwstk.edu.pl>
2025-04-27 12:29:04 +0200

    Project added

---
+++ tutt7/src/Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    public void makeNoise(){
+        System.out.println("Animal making noise");
+    }
+
+    public void roam(){
+        System.out.println("Animal roaming");
+    }
+}
